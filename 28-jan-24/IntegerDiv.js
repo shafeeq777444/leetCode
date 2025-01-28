e6342812f7deb99b9f04ed1778ae41d7c79a6dfc
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-01-28 09:32:19 +0530

    Add files via upload

---
+++ 28-jan-24/IntegerDiv.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    const ans=Math.trunc(dividend/divisor)
+    return ans
+};
+
+
+console.log(divide(7,3))
+
+
